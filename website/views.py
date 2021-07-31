69b12b37cbc05a87d18af83446a412e6d77609fa
Felipe500 <felipe.brx.dev@gmail.com>
2021-07-30 22:31:45 -0300

    atualização 1

--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def hello_blog(request):
+	#return HttpResponse('BLOG')
+	lista = [
+		'Django', 'Python', 'Apache', 'Git', 'Html'
+		'Banco de Dados ', 'Linux ', 'Nginx'
+	]
+	
+	valor1 = '1'
+	data = { 'var1': valor1, 'lista_tec': lista}
+	
+	
+	return render(request, 'index.html',data)
