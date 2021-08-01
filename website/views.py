62218adfb0a2353a113536428045a112abdeaf44
Felipe500 <felipe.brx.dev@gmail.com>
2021-07-31 23:28:25 -0300

    Carregar dados do banco de dados

--- website/views.py
+++ website/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 def hello_blog(request):
 	#return HttpResponse('BLOG')
+	valor1 = '1'
 	lista = [
 		'Django', 'Python', 'Apache', 'Git', 'Html',
 		'Banco de Dados ', 'Linux ', 'Nginx', 'PHP', 'CSS'
 	]
-	
-	valor1 = '1'
-	data = { 'var1': valor1, 'lista_tec': lista}
+	#--------------------------------
+	lista_postagens = Post.objects.all()
+	#------------------------------------
+	data = { 'var1': valor1, 'lista_tec': lista, 'post': lista_postagens}
 	
 	
 	return render(request, 'index.html',data)
